fa0985c5497d237ee1a24ed965550c5393874106
charlyk <saikmaxim@yandex.ru>
2026-04-06 13:25:13 +0300

    код для библиотеки mpu6050-raspberrypi

---
+++ main.py
@@ -0,0 +1,17 @@
+import time
+
+mpu6050 = mpu6050.mpu6050(0x68)
+
+def read_sensor_data():
+    acc_data = mpu6050.get_accel_data()
+    gyro_data = mpu6050.get_gyro_data()
+    t = mpu6050.get_temp()
+
+    return acc_data, gyro_data, t
+
+while True:
+    acc_data, gyro_data, t = read_sensor_data()
+    print(f"Acceleration: {acc_data}")
+    print(f"Gyro: {gyro_data}")
+    print(f"Temperature: {t}")
+    time.sleep(0.1)
